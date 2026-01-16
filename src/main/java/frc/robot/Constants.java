cb5bf599c36849383e47254f7c71fc598c68ed4a
Amogh90 <hgomam@gmail.com>
2026-01-15 21:22:38 -0500

    untested vision stuff

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,130 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+import dev.doglog.DogLog;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Unit;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+
+import edu.wpi.first.math.util.Units;
+import java.lang.annotation.Documented;
+import java.lang.reflect.Array;
+import java.rmi.MarshalException;
+import java.text.CollationElementIterator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.math.util.Units;
+
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  // public static double shootAlgaeDistance = 2; // m from center of field
+  
+public static class Vision {
+
+        public static boolean DOGLOG_ENABLED = false;
+
+        public static final boolean USE_VISION = true;
+
+        public static final String kCameraNameFront = "LeftCamera";
+        public static final Transform3d kRobotToCamFront =
+                new Transform3d(new Translation3d(Units.inchesToMeters(14 - 5.03), Units.inchesToMeters((12 - 5.56)), Units.inchesToMeters(17.00)), 
+                new Rotation3d(Math.toRadians(-0), Math.toRadians(20), Math.toRadians(15))); //TODO: determine XYZ
+
+        public static final String kCameraNameBack = "RightCamera";
+        public static final Transform3d kRobotToCamBack =
+                new Transform3d(new Translation3d(Units.inchesToMeters(14 - 5.03), -Units.inchesToMeters((12 - 5.56)), Units.inchesToMeters(17.00)), 
+                new Rotation3d(Math.toRadians(-0), Math.toRadians(20), Math.toRadians(-15))); //TODO: determine XYZ
+
+        // The layout of the AprilTags on the field
+        public static final AprilTagFieldLayout kTagLayout =
+                AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+
+        //Do not change these. Actual values will be calculated by the vision system.
+        public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
+
+        public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
+        
+        //Change these for fine tune vision system calculations of standard deviations.
+        public static final double kXYStdDev = 0.4; 
+        public static final double kThetaStdDev = 1; 
+
+        public static final double TRANSLATION_TOLERANCE_X = 0.013; // Changed from 0.05 3/8/25
+        public static final double TRANSLATION_TOLERANCE_Y = 0.013; // Changed from 0.05 3/8/25
+        public static final double ROTATION_TOLERANCE = Math.toRadians(1.3); // /deg
+
+        //Below same as pathplanner constants
+        public static final double MAX_VELOCITY = 3; 
+        public static final double MAX_ACCELERATION = 5; 
+        public static final double MAX_VELOCITY_ROTATION = 540; 
+        public static final double MAX_ACCELARATION_ROTATION = 720;
+        
+        public static final double VELOCITY_TOLERANCE_X = 4;
+        public static final double VELOCITY_TOLERANCE_Y = 4;
+        public static final double VELOCITY_TOLERANCE_OMEGA = 5;
+        public static final double kPXController = 10; //2.5
+        public static final double kIXController = 0.01d ; //0.01d
+        public static final double kDXController = 0d;
+        public static final double kIzoneX = 1.0d;
+        public static final double kIzoneY = 1.0d;
+        public static final double kPThetaController = 0.5; //2
+        public static final double kIThetaController = 0.0;
+        public static final double kDThetaController = 0.0; //0.0041
+        public static final double IZone = 1.0d;
+        // public static final double autoTurnCeiling = 5.0;
+
+        public static final double kPoseAmbiguityThreshold = 0.2;
+        public static final double kSingleTagDistanceThreshold = 2.0;
+
+        
+    }
+
+    public static final double gyroP = 2;
+    public static final double gyroI = 0.0;
+    public static final double gyroD = 0.00;
+
+    public static final String pigeonCanBus = "canivore1";
+
+
+//         for (int i = 0; i < FieldConstants.Reef.branchPositions.size(); i++) {
+//           for (FieldConstants.ReefHeight height : FieldConstants.ReefHeight.values()) {
+//             DogLog.log("Target Pose "+ i + " " + height.toString(), FieldConstants.Reef.branchPositions.get(i).get(height).toPose2d());
+          
+//         }
+//       }
+//     }
+  
+}
---
+++ src/main/java/frc/robot/subsystems/PoseEstimatorSubsystem.java
@@ -0,0 +1,197 @@
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.Vision.USE_VISION;
+import static frc.robot.Constants.Vision.kCameraNameBack;
+import static frc.robot.Constants.Vision.kCameraNameFront;
+import static frc.robot.Constants.Vision.kRobotToCamBack;
+import static frc.robot.Constants.Vision.kRobotToCamFront;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+
+import com.ctre.phoenix6.SignalLogger;
+import com.ctre.phoenix6.Utils;
+
+import dev.doglog.DogLog;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Robot;
+
+
+public class PoseEstimatorSubsystem extends SubsystemBase {
+
+    private final CommandSwerveDrivetrain driveTrain;
+    public Vision visionFront;
+    public Vision visionBack;
+    private static Notifier allNotifier;
+
+    Pose2d robotPose2d = new Pose2d();
+    StructPublisher<Pose2d> publisher;
+
+
+    //private final Pigeon2 gyro = new Pigeon2(TunerConstants.kPigeonId);
+    //private PIDController GyroPID = new PIDController(Constants.gyroP, Constants.gyroI, Constants.gyroD);
+    //public double targetAngle = 0;
+    //private Rotation2d gyroResetAngle = new Rotation2d();
+
+       
+    private Field2d field = new Field2d(); 
+          
+        // Simulation
+    
+        public PoseEstimatorSubsystem(CommandSwerveDrivetrain driveTrain) {
+            this.driveTrain = driveTrain;
+            if(USE_VISION) {
+    
+                this.visionFront = new Vision(kCameraNameFront, kRobotToCamFront, driveTrain);
+                this.visionBack = new Vision(kCameraNameBack, kRobotToCamBack, driveTrain);
+    
+                allNotifier = new Notifier(() -> {
+                    visionFront.run();
+                    visionBack.run();
+                });
+    
+                allNotifier.setName("runAll");
+                allNotifier.startPeriodic(0.02);
+
+                publisher = NetworkTableInstance.getDefault()
+                .getStructTopic("Robot Pose AdvScope", Pose2d.struct).publish();
+              
+        }        
+
+    }
+
+    @Override
+    public void periodic() {
+        // Update pose estimator with drivetrain sensors
+        if(USE_VISION) {
+             Optional<EstimatedRobotPose> visionEstFront  = visionFront.getEstimatedRobotPose();
+            visionEstFront.ifPresent(
+                est -> {
+                    // Change our trust in the measurement based on the tags we can see
+                    var estStdDevs = visionFront.getEstimationStdDevs();
+                    SignalLogger.writeBoolean("front refreshed", true);
+
+                    // printMatrixValues(estStdDevs);
+                    driveTrain.addVisionMeasurement(
+                            est.estimatedPose.toPose2d(), Utils.fpgaToCurrentTime(est.timestampSeconds), estStdDevs);
+
+                });
+
+            Optional<EstimatedRobotPose> visionEstBack  = visionBack.getEstimatedRobotPose();
+
+            visionEstBack = visionBack.getEstimatedRobotPose();
+            visionEstBack.ifPresent(
+                est -> {
+                // Change our trust in the measurement based on the tags we can see
+                var estStdDevs = visionBack.getEstimationStdDevs();               
+                SignalLogger.writeBoolean("front refreshed", true);
+
+                driveTrain.addVisionMeasurement(
+                        est.estimatedPose.toPose2d(), Utils.fpgaToCurrentTime(est.timestampSeconds), estStdDevs);
+
+                });
+
+            if(Robot.isSimulation() ) {
+                 visionFront.simulationPeriodic(getCurrentPose());
+                // SmartDashboard.putData("Debug Field Front", visionFront.getSimDebugField());
+                 visionBack.simulationPeriodic(getCurrentPose());
+                // SmartDashboard.putData("Debug Field Back", visionBack.getSimDebugField());
+    
+            }
+
+            if(visionEstFront.isPresent() && Constants.Vision.DOGLOG_ENABLED) {
+               DogLog.log("PoseEstimator/VisionEst", visionEstFront.get().estimatedPose.toPose2d());
+            }
+
+            if(visionEstBack.isPresent() && Constants.Vision.DOGLOG_ENABLED) {
+                DogLog.log("PoseEstimator/VisionEst", visionEstBack.get().estimatedPose.toPose2d());
+             }
+
+             
+        }
+        else {
+            if (allNotifier != null) allNotifier.close();
+        }
+
+        if (getCurrentPose() != null) {
+            field.setRobotPose(getCurrentPose());
+            robotPose2d = getCurrentPose();
+            publisher.set(robotPose2d);
+
+            // field.getObject("VisionEstimation").setPoses();
+
+            SmartDashboard.putData("Robot Pose in Field", field);
+            SmartDashboard.putString("Formatted Pose", getFomattedPose());
+
+            // DogLog.log("PoseEstimator/Pose", getCurrentPose());
+            // DogLog.log("PoseEstimator/Formatted Pose", getFomattedPose());            
+
+        }
+    }
+    
+
+    private String getFomattedPose() {
+        var pose = getCurrentPose();
+        return String.format(
+                "(%.3f, %.3f) %.2f degrees",
+                pose.getX(), pose.getY(), pose.getRotation().getDegrees());
+    }
+
+    private String getFomattedPose(Pose2d pose) {
+        return String.format(
+                "(%.3f, %.3f) %.2f degrees",
+                pose.getX(), pose.getY(), pose.getRotation().getDegrees());
+    }
+
+    public Pose2d getCurrentPose() {
+        return driveTrain.getState().Pose;
+    }
+
+    /**
+     * Resets the current pose to the specified pose. This should ONLY be called
+     * when the robot's position on the field is known, like at the beginning of
+     * a match.
+     *
+     * @param newPose new pose
+     */
+    public void setCurrentPose(Pose2d newPose) {
+        //driveTrain.seedFieldRelative(newPose);
+        driveTrain.resetPose(newPose);
+    }
+
+    /**
+     * Resets the position on the field to 0,0 0-degrees, with forward being
+     * downfield. This resets
+     * what "forward" is for field oriented driving.
+     */
+    public void resetFieldPosition() {
+        setCurrentPose(new Pose2d());
+    }
+
+    public void printMatrixValues(Matrix<N3, N1> curStdDevs) {
+        
+        for (int i = 0; i < curStdDevs.getNumRows(); i++) {
+            for (int j = 0; j < curStdDevs.getNumCols(); j++) {
+                DogLog.log("Stddev "+ i,curStdDevs.get(i, j));
+            }
+        }
+    }
+}
