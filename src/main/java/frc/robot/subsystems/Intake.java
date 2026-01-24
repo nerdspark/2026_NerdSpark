944c055e7bb8928980ab1407752c399d9ba56631
Sarvad Sista <sarvadsista@gmail.com>
2026-01-23 19:40:54 -0500

    added intake command and fixed some problems with the subsystems

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,22 +1,27 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 
-public class Intake {
+public class Intake implements Subsystem{
     private TalonFX intakeMotorRoller1;
     private TalonFX intakeMotorRoller2;
     private TalonFX intakeMotorDeploy;
 
-    private int intakeID = 0;
+    private int intakeMotorRoller1ID;
+    private int intakeMotorRoller2ID;
+    private int intakeMotorDeployID;
     
 
     public Intake() {
-        intakeMotorRoller1 = new TalonFX(intakeID, "intakeMotorRoller1  ");
-        intakeMotorRoller2 = new TalonFX(intakeID, "intakeMotorRoller2");
-        intakeMotorDeploy = new TalonFX(intakeID, "intakeMotorDeploy");
+        intakeMotorRoller1 = new TalonFX(intakeMotorRoller1ID, "intakeMotorRoller1  ");
+        intakeMotorRoller2 = new TalonFX(intakeMotorRoller2ID, "intakeMotorRoller2");
+        intakeMotorDeploy = new TalonFX(intakeMotorDeployID, "intakeMotorDeploy");
 
         TalonFXConfiguration intakeDeployMotorConfig = new TalonFXConfiguration();
         TalonFXConfiguration intakeMotorRoller1 = new TalonFXConfiguration();
