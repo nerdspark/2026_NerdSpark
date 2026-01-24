12d1ee2ff5628ede0f43b52ea2ca98d5b2ee83b4
Rohan <rnatogi@gmail.com>
2026-01-23 19:56:50 -0500

    removed third motor

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -19,14 +19,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimbConstants;
 
 public class Climb extends SubsystemBase {
-    private TalonFX climbRight, climbLeft, climbHook;
+    private TalonFX climbRight, climbLeft;
     private TalonFXConfiguration climbConfig = new TalonFXConfiguration();
     private boolean ampTriggered, ampTriggerStarted = false;
   /** Creates a new Climb. */
   public Climb() {
     climbLeft = new TalonFX(ClimbConstants.kLeftID, ClimbConstants.canBus);
     climbRight = new TalonFX(ClimbConstants.kRightID, ClimbConstants.canBus);
-    climbHook = new TalonFX(ClimbConstants.kHookID, ClimbConstants.canBus);
+    // climbHook = new TalonFX(ClimbConstants.kHookID, ClimbConstants.canBus);
 //Initializing the motor
     climbConfig.CurrentLimits = new CurrentLimitsConfigs()
           .withStatorCurrentLimit(ClimbConstants.climbCurrentLimit)
@@ -49,11 +49,11 @@ public class Climb extends SubsystemBase {
           .apply(climbConfig.withMotorOutput(new MotorOutputConfigs()
           .withInverted(InvertedValue.Clockwise_Positive)
               .withNeutralMode(NeutralModeValue.Brake)));
-      climbHook
-        .getConfigurator()
-            .apply(climbConfig.withMotorOutput(new MotorOutputConfigs()
-            .withInverted(InvertedValue.Clockwise_Positive)
-                .withNeutralMode(NeutralModeValue.Brake)));
+      // climbHook
+      //   .getConfigurator()
+      //       .apply(climbConfig.withMotorOutput(new MotorOutputConfigs()
+      //       .withInverted(InvertedValue.Clockwise_Positive)
+      //           .withNeutralMode(NeutralModeValue.Brake)));
       
       resetPosition();
   }
@@ -64,7 +64,7 @@ public class Climb extends SubsystemBase {
   public void setPosition (double position){
     climbLeft.setControl(new PositionVoltage(position));
     climbRight.setControl(new PositionVoltage(position));
-    climbHook.setControl(new PositionVoltage(position));
+    // climbHook.setControl(new PositionVoltage(position));
 
   }
 
@@ -82,4 +82,3 @@ public class Climb extends SubsystemBase {
   }
 }
 
-//skibidi2
